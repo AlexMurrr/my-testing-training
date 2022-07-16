84afabd09839114a24a4e5a3edc40460499547c4
Alex Mur <lehamuraschov2014@yandex.ru>
2022-07-16 08:40:57 +0200

    test: add test for string and contain

--- arrayForTest.test.js
+++ arrayForTest.test.js
@@ -2,8 +2,26 @@ const fruitT = require( "./arrayForTest")
 
 let fruit = fruitT;
 
+let checkForString = (array) => {
+    for(let item of array){
+        if(typeof item === 'string'){
+            return true
+        }else return false
+        
+    }
+}
+
 test ('extend is array', ()=>{    
-    expect(fruit).toStrictEqual(["banana", "banana", "apple", "orange", "apple"])
+    expect(fruit).toStrictEqual(["banana", "banana", "apple", "orange", "apple"]);
 })
 
-test()
+test ('check all item for string', ()=>{
+    expect(checkForString(fruit)).toBeTruthy();
+})
+
+test ('containe item', ()=>{
+    expect(fruit).toContain('apple');
+})
+
+
+
