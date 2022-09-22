a5516621c8a455a75b5ac3f253cc3e36d1f6aa89
Alex Mur <lehamuraschov2014@yandex.ru>
2022-09-22 06:33:00 +0200

    feat: create file promise.js

---
+++ promise.js
@@ -0,0 +1,30 @@
+
+
+const p = new Promise((resolve, reject) =>{
+    setTimeout(() => {
+        console.log('Preparing data...');
+        const backendData = {
+            server: 'avs',
+            port: 200,
+            status: 'working',
+        }
+        resolve(backendData);
+    }, 2000)
+})
+
+p.then(data => {
+    return new Promise((resolve, reject) =>{
+    setTimeout(() => {
+            data.modified = true;
+            resolve(data);
+        },2000)
+    })
+})
+.then(clientData => {
+    console.log('Data recieved', clientData);
+    clientData.formatPromis = true;
+    return clientData;
+})
+.then(data => {
+    console.log('Modified', data);
+})
